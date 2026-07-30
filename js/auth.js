ff2f79820481584c966b24225d83f09ace2e5b0d
ainokori <eugene.aquila06@gmail.com>
2026-07-30 10:01:53 +0800

    Delete auth.js

--- js/auth.js
+++
@@ -1,6 +0,0 @@
-async function getProfile(){
- const {data:{user}} = await supabaseClient.auth.getUser();
- if(!user) return null;
- const {data} = await supabaseClient.from('profiles').select('*').eq('id',user.id).single();
- return data;
-}
