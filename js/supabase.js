d77cc050f3c43f418d956cff7d50c42afa6b3a7a
ainokori <eugene.aquila06@gmail.com>
2026-07-30 10:01:56 +0800

    Delete supabase.js

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-// Supabase client bootstrap
-const SUPABASE_URL = "https://yswwicdycbscurnpbqjb.supabase.co";
-const SUPABASE_ANON_KEY = "REPLACE_WITH_YOUR_ANON_KEY";
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
